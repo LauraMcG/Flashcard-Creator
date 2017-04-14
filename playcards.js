b8b7aa3edc22cb67fff30b42d50ef237268cc5ef
Laura McGinn <mcginn.laura@gmail.com>
2017-04-13 23:00:41 -0500

    initializing the play interface

---
+++ playcards.js
@@ -0,0 +1,11 @@
+// var inquirer = require ('inquirer');
+
+var fs = require ('fs');
+
+var basicQuestions = require('./basic-cards.json');
+
+// var basicQuestions = require ('./basic-cards.json');
+
+// console.log(basicQuestions[0].back);
+
+console.log(basicQuestions);
