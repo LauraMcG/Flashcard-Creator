710e7dbf9dd23a19ba830838864c726afbde2da5
Laura McGinn <mcginn.laura@gmail.com>
2017-04-13 23:25:48 -0500

    adding package.json and .gitignore

--- playcards.js
+++ playcards.js
@@ -1,11 +1,17 @@
 // var inquirer = require ('inquirer');
 
 var fs = require ('fs');
-
 var basicQuestions = require('./basic-cards.json');
+var basicQuestions = require ('./basic-cards.json');
 
-// var basicQuestions = require ('./basic-cards.json');
+var questionNumber = 0;
+var questionsRight = 0;
+var questionsWrong = 0;
 
 // console.log(basicQuestions[0].back);
 
-console.log(basicQuestions);
+// console.log(basicQuestions);
+
+for (var i = 0; i < basicQuestions.length; i++) {
+	console.log(basicQuestions[i].back);
+}
