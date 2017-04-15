6ed616ad13b2b4077ab251f183c0b67155786d3b
Laura McGinn <mcginn.laura@gmail.com>
2017-04-14 20:35:05 -0500

    progress on playcards

--- playcards.js
+++ playcards.js
@@ -1,17 +1,76 @@
-// var inquirer = require ('inquirer');
-
+var inquirer = require ('inquirer');
 var fs = require ('fs');
 var basicQuestions = require('./basic-cards.json');
-var basicQuestions = require ('./basic-cards.json');
+var clozeQuestions = require ('./cloze-cards.json');
 
+//game state variables
 var questionNumber = 0;
 var questionsRight = 0;
 var questionsWrong = 0;
+
+//BASIC QUIZ
+
+function basicQuiz() {
+
+	//cardQuestions sets up the basic template for questions.
+	var cardQuestions = [
+		{
+			type: 'input',
+			name: 'question',
+			message: basicQuestions[questionNumber].front
+		}
+	];
+
+	function ask() {
+		console.log(questionNumber);
+		inquirer.prompt(cardQuestions).then(function(user) {
+			if (user.question == basicQuestions[questionNumber].back) {
+				questionNumber++;
+				console.log ('correct!');	
+			} else {
+				console.log('incorrect!');
+				questionNumber++;
+			}
+
+			//checking to see whether to move on to the next question
+			if (questionNumber < basicQuestions.length) {
+				ask();
+			} else {
+				console.log('you finished!');
+			}
+		});
+	}
+
+	ask();	
+}
+
+
+//CLOZE QUIZ
+
+function clozeQuiz() {
+	console.log('cloze quiz works!');
+}
+
+
+// Opening question. User chooses which kind of questions to answer.
+inquirer.prompt([
+	{
+		type: 'list',
+		name: 'quizType',
+		message: 'what kind of flash cards do you want to answer?',
+		choices: ['Basic', 'Cloze']
+	}
+
+]).then (function(answer) {
+	if (answer.quizType === 'Basic') {
+		basicQuiz();
+	} else if (answer.quizType === 'Cloze') {
+		clozeQuiz();
+	}
+
+});
 
 // console.log(basicQuestions[0].back);
 
 // console.log(basicQuestions);
 
-for (var i = 0; i < basicQuestions.length; i++) {
-	console.log(basicQuestions[i].back);
-}
